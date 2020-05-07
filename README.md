a28242a7b0948239a2925ccdbe9522fc676cebdd
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-05-07 16:40:17 +1000

    Init

---
+++ README.md
@@ -0,0 +1,26 @@
+offregister_python_venv
+=======================
+This package follows the offregister specification to handle python virtual environments (venvs), including inline to offregister execution within the venv.
+
+## Install dependencies
+
+    pip install -r requirements.txt
+
+## Install package
+
+    pip install .
+
+## License
+
+Licensed under either of
+
+- Apache License, Version 2.0 ([LICENSE-APACHE](LICENSE-APACHE) or <https://www.apache.org/licenses/LICENSE-2.0>)
+- MIT license ([LICENSE-MIT](LICENSE-MIT) or <https://opensource.org/licenses/MIT>)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted
+for inclusion in the work by you, as defined in the Apache-2.0 license, shall be
+dual licensed as above, without any additional terms or conditions.
---
+++ offregister_python_venv/ubuntu/__init__.py
@@ -0,0 +1,96 @@
+from functools import partial
+from itertools import imap
+
+from fabric.api import run
+from fabric.context_managers import shell_env, cd
+from fabric.contrib.files import exists
+from fabric.operations import sudo, _run_command
+from offregister_fab_utils.apt import apt_depends
+from offregister_fab_utils.python import pip_depends
+from six import string_types
+
+
+def install_venv0(python3=False, venv=None, *args, **kwargs):
+    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo', False))
+
+    ensure_pip_version = lambda: kwargs.get('pip_version') and sudo(
+        'pip install pip '.format('== {}'.format(kwargs['pip_version'] if 'pip_version' in kwargs else '-U')))
+
+    venv_dir = kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True))
+    venv = venv or '{venv_dir}/{venv_name}'.format(
+        venv_dir=venv_dir, venv_name=kwargs.get('VENV_NAME', 'venv')
+    )
+
+    if exists('{}/bin/python'.format(venv)):
+        return
+    elif python3:
+        apt_depends('python3-dev', 'python3-pip', 'python3-wheel', 'python3-venv')
+    else:
+        apt_depends('python-dev', 'python-pip', 'python-wheel', 'python2.7', 'python2.7-dev')  # 'python-apt'
+        sudo('pip install virtualenv')
+
+    virtual_env_bin = '{venv}/bin'.format(venv=venv)
+    if not exists(virtual_env_bin):
+        sudo('mkdir -p "{venv_dir}"'.format(venv_dir=venv_dir),
+             shell_escape=False)
+        if python3:
+            sudo('python3 -m venv "{venv}"'.format(venv=venv),
+                 shell_escape=False)
+        else:
+            sudo('virtualenv "{venv}"'.format(venv=venv), shell_escape=False)
+
+    if not exists(virtual_env_bin):
+        raise ReferenceError('Virtualenv does not exist')
+
+    if not kwargs.get('use_sudo'):
+        user_group = run('echo $(id -un):$(id -gn)', quiet=True)
+        sudo('chown -R {user_group} {venv} $HOME/.cache'.format(user_group=user_group,
+                                                                       venv=venv))
+
+    with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)):
+        ensure_pip_version()
+        run_cmd('pip install -U wheel setuptools')
+        return 'Installed: {} {}'.format(
+            run_cmd('pip --version; python --version'),
+            pip_depends('{}/bin/python'.format(venv),
+                        kwargs.get('use_sudo', False),
+                        kwargs.get('PACKAGES', tuple()))
+        )
+
+
+def run_inside1(package_directory=None, venv=None, requirements=True, *args, **kwargs):
+    if package_directory is None or not exists(package_directory):
+        return
+    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo'))
+    venv = venv or '{venv_dir}/{venv_name}'.format(
+        venv_dir=kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True)),
+        venv_name=kwargs.get('VENV_NAME', 'venv')
+    )
+    with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)), cd(package_directory):
+        requirements = 'requirements.txt' if requirements is True else requirements
+        if requirements:
+            if isinstance(requirements, list):
+                map(lambda req: run_cmd('pip install -r "{}"'.format(req)),
+                    requirements)
+            else:
+                run_cmd('pip install -r "{}"'.format(requirements))
+
+        return run_cmd('pip uninstall -y "${PWD##*/}"; pip install .;')
+
+
+def run_within_venv2(venv=None, venv_execute=None, *args, **kwargs):
+    if venv_execute is None:
+        venv_execute = kwargs.get('VENV_EXECUTE')
+        if venv_execute is None:
+            return
+
+    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo'))
+    venv = venv or '{venv_dir}/{venv_name}'.format(
+        venv_dir=kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True)),
+        venv_name=kwargs.get('VENV_NAME', 'venv')
+    )
+    with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)):
+        if isinstance(venv_execute, string_types):
+            return run_cmd(venv_execute)
+
+        return tuple(imap(run_cmd, venv_execute))
