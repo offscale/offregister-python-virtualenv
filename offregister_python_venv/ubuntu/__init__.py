c82a5ed64fbbff8174cf133ad2d2136833691504
Samuel Marks <807580+SamuelMarks@users.noreply.github.com>
2020-09-21 18:31:40 +1000

    [*.py] Fix Python 2 support

--- offregister_python_venv/ubuntu/__init__.py
+++ offregister_python_venv/ubuntu/__init__.py
@@ -1,5 +1,10 @@
+from collections import deque
 from functools import partial
 from itertools import imap
+from sys import version
+
+if version[0] == "2":
+    from itertools import imap as map
 
 from fabric.api import run
 from fabric.context_managers import shell_env, cd
@@ -11,67 +16,78 @@ from six import string_types
 
 
 def install_venv0(python3=False, venv=None, *args, **kwargs):
-    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo', False))
+    run_cmd = partial(_run_command, sudo=kwargs.get("use_sudo", False))
 
-    ensure_pip_version = lambda: kwargs.get('pip_version') and sudo(
-        'pip install pip '.format('== {}'.format(kwargs['pip_version'] if 'pip_version' in kwargs else '-U')))
-
-    venv_dir = kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True))
-    venv = venv or '{venv_dir}/{venv_name}'.format(
-        venv_dir=venv_dir, venv_name=kwargs.get('VENV_NAME', 'venv')
+    ensure_pip_version = lambda: kwargs.get("pip_version") and sudo(
+        "pip install pip ".format(
+            "== {}".format(kwargs["pip_version"] if "pip_version" in kwargs else "-U")
+        )
     )
 
-    if exists('{}/bin/python'.format(venv)):
+    venv_dir = kwargs.get("VENV_DIR", run("echo $HOME/venvs", quiet=True))
+    venv = venv or "{venv_dir}/{venv_name}".format(
+        venv_dir=venv_dir, venv_name=kwargs.get("VENV_NAME", "venv")
+    )
+
+    if exists("{}/bin/python".format(venv)):
         return
     elif python3:
-        apt_depends('python3-dev', 'python3-pip', 'python3-wheel', 'python3-venv')
+        apt_depends("python3-dev", "python3-pip", "python3-wheel", "python3-venv")
     else:
-        apt_depends('python-dev', 'python-pip', 'python-wheel', 'python2.7', 'python2.7-dev')  # 'python-apt'
-        sudo('pip install virtualenv')
+        apt_depends(
+            "python-dev", "python-pip", "python-wheel", "python2.7", "python2.7-dev"
+        )  # 'python-apt'
+        sudo("pip install virtualenv")
 
-    virtual_env_bin = '{venv}/bin'.format(venv=venv)
+    virtual_env_bin = "{venv}/bin".format(venv=venv)
     if not exists(virtual_env_bin):
-        sudo('mkdir -p "{venv_dir}"'.format(venv_dir=venv_dir),
-             shell_escape=False)
+        sudo('mkdir -p "{venv_dir}"'.format(venv_dir=venv_dir), shell_escape=False)
         if python3:
-            sudo('python3 -m venv "{venv}"'.format(venv=venv),
-                 shell_escape=False)
+            sudo('python3 -m venv "{venv}"'.format(venv=venv), shell_escape=False)
         else:
             sudo('virtualenv "{venv}"'.format(venv=venv), shell_escape=False)
 
     if not exists(virtual_env_bin):
-        raise ReferenceError('Virtualenv does not exist')
+        raise ReferenceError("Virtualenv does not exist")
 
-    if not kwargs.get('use_sudo'):
-        user_group = run('echo $(id -un):$(id -gn)', quiet=True)
-        sudo('chown -R {user_group} {venv} $HOME/.cache'.format(user_group=user_group,
-                                                                       venv=venv))
+    if not kwargs.get("use_sudo"):
+        user_group = run("echo $(id -un):$(id -gn)", quiet=True)
+        sudo(
+            "chown -R {user_group} {venv} $HOME/.cache".format(
+                user_group=user_group, venv=venv
+            )
+        )
 
     with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)):
         ensure_pip_version()
-        run_cmd('pip install -U wheel setuptools')
-        return 'Installed: {} {}'.format(
-            run_cmd('pip --version; python --version'),
-            pip_depends('{}/bin/python'.format(venv),
-                        kwargs.get('use_sudo', False),
-                        kwargs.get('PACKAGES', tuple()))
+        run_cmd("pip install -U wheel setuptools")
+        return "Installed: {} {}".format(
+            run_cmd("pip --version; python --version"),
+            pip_depends(
+                "{}/bin/python".format(venv),
+                kwargs.get("use_sudo", False),
+                kwargs.get("PACKAGES", tuple()),
+            ),
         )
 
 
 def run_inside1(package_directory=None, venv=None, requirements=True, *args, **kwargs):
     if package_directory is None or not exists(package_directory):
         return
-    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo'))
-    venv = venv or '{venv_dir}/{venv_name}'.format(
-        venv_dir=kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True)),
-        venv_name=kwargs.get('VENV_NAME', 'venv')
+    run_cmd = partial(_run_command, sudo=kwargs.get("use_sudo"))
+    venv = venv or "{venv_dir}/{venv_name}".format(
+        venv_dir=kwargs.get("VENV_DIR", run("echo $HOME/venvs", quiet=True)),
+        venv_name=kwargs.get("VENV_NAME", "venv"),
     )
-    with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)), cd(package_directory):
-        requirements = 'requirements.txt' if requirements is True else requirements
+    with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)), cd(
+        package_directory
+    ):
+        requirements = "requirements.txt" if requirements is True else requirements
         if requirements:
             if isinstance(requirements, list):
-                map(lambda req: run_cmd('pip install -r "{}"'.format(req)),
-                    requirements)
+                deque(map(
+                    lambda req: run_cmd('pip install -r "{}"'.format(req)), requirements
+                ), maxlen=0)
             else:
                 run_cmd('pip install -r "{}"'.format(requirements))
 
@@ -80,14 +96,14 @@ def run_inside1(package_directory=None, venv=None, requirements=True, *args, **k
 
 def run_within_venv2(venv=None, venv_execute=None, *args, **kwargs):
     if venv_execute is None:
-        venv_execute = kwargs.get('VENV_EXECUTE')
+        venv_execute = kwargs.get("VENV_EXECUTE")
         if venv_execute is None:
             return
 
-    run_cmd = partial(_run_command, sudo=kwargs.get('use_sudo'))
-    venv = venv or '{venv_dir}/{venv_name}'.format(
-        venv_dir=kwargs.get('VENV_DIR', run('echo $HOME/venvs', quiet=True)),
-        venv_name=kwargs.get('VENV_NAME', 'venv')
+    run_cmd = partial(_run_command, sudo=kwargs.get("use_sudo"))
+    venv = venv or "{venv_dir}/{venv_name}".format(
+        venv_dir=kwargs.get("VENV_DIR", run("echo $HOME/venvs", quiet=True)),
+        venv_name=kwargs.get("VENV_NAME", "venv"),
     )
     with shell_env(VIRTUAL_ENV=venv, PATH="{}/bin:$PATH".format(venv)):
         if isinstance(venv_execute, string_types):
--- setup.py
+++ setup.py
@@ -1,44 +1,52 @@
 from setuptools import setup, find_packages
+from sys import version
+
+if version[0] == "2":
+    from itertools import imap as map, ifilter as filter
 from os import path, listdir
 from functools import partial
 from itertools import imap, ifilter
 from ast import parse
 from distutils.sysconfig import get_python_lib
 
-if __name__ == '__main__':
-    package_name = 'offregister_python_venv'
+if __name__ == "__main__":
+    package_name = "offregister_python_venv"
 
-    with open(path.join(package_name, '__init__.py')) as f:
+    with open(path.join(package_name, "__init__.py")) as f:
         __author__, __version__ = imap(
             lambda buf: next(imap(lambda e: e.value.s, parse(buf).body)),
-            ifilter(lambda line: line.startswith('__version__') or line.startswith('__author__'), f)
+            ifilter(
+                lambda line: line.startswith("__version__")
+                or line.startswith("__author__"),
+                f,
+            ),
         )
 
-    to_funcs = lambda *paths: (partial(path.join, path.dirname(__file__), package_name, *paths),
-                               partial(path.join, get_python_lib(prefix=''), package_name, *paths))
-    _data_join, _data_install_dir = to_funcs('_data')
+    to_funcs = lambda *paths: (
+        partial(path.join, path.dirname(__file__), package_name, *paths),
+        partial(path.join, get_python_lib(prefix=""), package_name, *paths),
+    )
+    _data_join, _data_install_dir = to_funcs("_data")
 
     setup(
         name=package_name,
         author=__author__,
         version=__version__,
-        description='Python deployment module for Fabric (offregister)',
+        description="Python deployment module for Fabric (offregister)",
         classifiers=[
-            'Development Status :: 7 - Inactive',
-            'Intended Audience :: Developers',
-            'Topic :: Software Development',
-            'Topic :: Software Development :: Libraries :: Python Modules',
-            'License :: OSI Approved :: MIT License',
-            'License :: OSI Approved :: Apache Software License',
-            'Programming Language :: Python',
-            'Programming Language :: Python :: 2.7',
-            'Programming Language :: Python :: 2 :: Only'
+            "Development Status :: 7 - Inactive",
+            "Intended Audience :: Developers",
+            "Topic :: Software Development",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+            "License :: OSI Approved :: MIT License",
+            "License :: OSI Approved :: Apache Software License",
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 2.7",
+            "Programming Language :: Python :: 2 :: Only",
         ],
-        install_requires=['pyyaml', 'fab-classic', 'paramiko'],
-        test_suite=package_name + '.tests',
+        install_requires=["pyyaml", "fab-classic", "paramiko"],
+        test_suite=package_name + ".tests",
         packages=find_packages(),
         package_dir={package_name: package_name},
-        data_files=[
-            (_data_install_dir(), map(_data_join, listdir(_data_join())))
-        ]
+        data_files=[(_data_install_dir(), map(_data_join, listdir(_data_join())))],
     )
